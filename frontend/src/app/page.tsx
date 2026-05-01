d6716089f02e8ec1245abcc1a27d50d0e620e2c8
Vincent <vincentperezlefeuvre@gmail.com>
2026-05-02 01:24:03 +0200

    feat: setup next.js and tailwind css with docker-compose

---
+++ frontend/src/app/page.tsx
@@ -0,0 +1,11 @@
+export default function Home() {
+  return (
+      <main className="flex min-h-screen flex-col items-center justify-center p-24 bg-gray-50">
+        <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
+          <h1 className="text-4xl font-bold text-blue-600">
+            UGEReveVue est en ligne !
+          </h1>
+        </div>
+      </main>
+  );
+}
